3b7e6faf91874bbc033f39415dd098b71da229d2
Andrey Kuzmin <andromed-ku@yandex.ru>
2024-07-18 19:01:41 +0500

    Update file

--- text_storage/text_storage/api/api.py
+++ text_storage/text_storage/api/api.py
@@ -1,8 +1,19 @@
+import uuid
+from datetime import datetime
+
 from starlette import status
 
+from text_storage.api.schemas import TextFragmentResponseSchema, TextFragmentSchema
 from text_storage.app import app
 
 
-@app.post("/api/{text}/store")
-def store_text(text, status_code=status.HTTP_201_CREATED):
+@app.post(
+    "/text/store",
+    status_code=status.HTTP_201_CREATED,
+    response_model=TextFragmentResponseSchema,
+)
+def store_text(text_detail: TextFragmentSchema):
+    text = text_detail.dict()
+    text["id"] = uuid.uuid4()
+    text["created_at"] = datetime.utcnow()
     return text
