02f4499fc7b34ef82822f40f95c6d19047464908
Andrey Kuzmin <andromed-ku@yandex.ru>
2024-07-18 18:58:36 +0500

    Overriding dynamically generated specification

--- text_storage/text_storage/app.py
+++ text_storage/text_storage/app.py
@@ -1,5 +1,13 @@
+from pathlib import Path
+
+import yaml
 from fastapi import FastAPI
 
-app: FastAPI = FastAPI(debug=True)
+app: FastAPI = FastAPI(
+    debug=True, openapi_url="/openapi/textdrop.json", docs_url="/docs/textdrop"
+)
+
+oas_doc = yaml.safe_load((Path(__file__).parent / "../oas.yaml").read_text())
+app.openapi = lambda: oas_doc
 
 from text_storage.api import api  # noqa: F401, E402
