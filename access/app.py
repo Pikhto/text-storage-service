3d3daa3413ec7619a457b10db91e50347b59a7ea
Andrey Kuzmin <andromed-ku@yandex.ru>
2024-07-27 19:22:23 +0500

    Create file app.py

---
+++ access/app.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import yaml
+from api.api import blueprint
+from apispec import APISpec
+from config import BaseConfig
+from flask import Flask
+from flask_smorest import Api
+
+app = Flask(__name__)
+app.config.from_object(BaseConfig)
+
+access_api = Api(app)
+access_api.register_blueprint(blueprint)
+
+api_spec = yaml.safe_load((Path(__file__).parent / "oas.yaml").read_text())
+spec = APISpec(
+    title=api_spec["info"]["title"],
+    version=api_spec["info"]["version"],
+    openapi_version=api_spec["openapi"],
+)
+spec.to_dict = lambda: api_spec
+access_api.spec = spec
